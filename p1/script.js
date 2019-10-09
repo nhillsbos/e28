470e2ce59eb0755c0e292b967bfe2ad3e881cc10
nhills <nhills@comcast.net>
2019-10-09 08:55:10 -0400

    finished rps game

--- p1/script.js
+++ p1/script.js
@@ -11,24 +11,27 @@ let myApp = new Vue({
         imgs: ['imgs/rock.svg','imgs/paper.svg','imgs/scissors.svg',]
     },
     methods: {
-        makeChoice: function (e) {
+        makeChoice: function (e) {//fires when player clicks choice, cpu
             this.playerChoice = e.target.id;
-            this.cpuChoice = this.rps[Math.floor(Math.random() * 3)];
+            this.cpuChoice = this.rps[Math.floor(Math.random() * 3)];//cpu random choice
             console.log('cpuChoice: ' + this.cpuChoice + ". playerChoice: " + this.playerChoice);
             let victor = this.victor;
+            //player wins
             if ((this.playerChoice == 'playerRock') && (this.cpuChoice == 'cpuScissors') || (this.playerChoice == 'playerPaper') && (this.cpuChoice == 'cpuRock') || 
             (this.playerChoice == 'playerScissors') && (this.cpuChoice == 'cpuPaper')) {
                 this.victor = 'player';
                 console.log(this.victor);
+                //cpu wins
             } else if ((this.playerChoice == 'playerRock') && (this.cpuChoice == 'cpuPaper') || (this.playerChoice == 'playerPaper') && (this.cpuChoice == 'cpuScissors') || 
             (this.playerChoice == 'playerScissors') && (this.cpuChoice == 'cpuRock')) {
                 this.victor = 'cpu';
                 console.log(this.victor);
+                //no winner
             } else {
                 this.victor = 'draw';
                 console.log(this.victor);
             }
-        }
+        },
     }
 });
 
